fef690e4fd47cb94e45798c327d2b3fc55ca5ee2
Kongkon-79 <kongkonjowarder@gmail.com>
2025-05-31 18:38:07 +0600

    auth setup

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './_components/LoginForm';
+
+const Login = () => {
+    return (
+        <div>
+            <LoginForm/>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/provider/AppProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </div>
+  );
+};
+
+export default AppProvider;
